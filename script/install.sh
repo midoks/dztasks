265833e477240a8a75d22ba40504711949b17790
Mr Chen <midoks@163.com>
2024-08-01 23:03:21 +0800

    Create install.sh

---
+++ script/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:/opt/homebrew/bin
+export PATH
+
+startTime=`date +%s`
+echo "安装中..."
+
+
+
+endTime=`date +%s`
+((outTime=(${endTime}-${startTime})/60))
+echo -e "安装耗时:\033[32m $outTime \033[0mMinute!"
+
+
