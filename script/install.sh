5eb54f45e2509cb3bb138cc3d1ddbc3463b99fca
Mr Chen <midoks@163.com>
2024-08-03 15:12:58 +0800

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -11,6 +11,34 @@ sysArch=`arch`
 
 echo $sysName, $sysArch
 
+VERSION='1.0'
+
+ARCH="amd64"
+if [ "$sysArch" == "x86_64" ];then
+	ARCH="amd64"
+elif [ "$sysArch" == "aarch64" ];then
+	ARCH="arm64"
+elif [ "$sysArch" == "arm64" ];then
+	ARCH="arm64"
+else
+	ARCH="amd64"
+fi
+
+SYSTEM=linux
+if [ "$sysName" == "Darwin" ];then
+	SYSTEM=darwin
+fi
+
+
+# https://github.com/midoks/dztasks/releases/download/1.0/dztasks_v1.0_linux_amd64.tar.gz
+DZTASKS_URL=https://github.com/midoks/dztasks/releases/download/${VERSION}
+FILE_NAME=dztasks_v${VERSION}_${SYSTEM}_${ARCH}.tar.gz
+TMP_DIR=/tmp
+
+if [ -f $TMP_DIR/${FILE_NAME} ];then
+	wget --no-check-certificate -O $TMP_DIR/${FILE_NAME} ${DZTASKS_URL}/${FILE_NAME}
+fi
+
 
 
 
